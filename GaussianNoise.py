ed46c14a4712f195b5e4d9f5b78aded45afd57c3
vitalex93 <vitalex93@gmail.com>
2022-06-26 22:35:37 +0300

    gaussian noise transform

--- GaussianNoise.py
+++ GaussianNoise.py
@@ -1,4 +1,6 @@
 import torch
+import torchvision
+from torchvision import transforms
 
 class AddGaussianNoise(object):
     def __init__(self, mean=0., std=1.):
@@ -10,3 +12,17 @@ class AddGaussianNoise(object):
     
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+def get_noisy_FashionMNIST_dataloaders(batch_size, mean, std):
+
+    train_transform = transforms.Compose([transforms.ToTensor(), AddGaussianNoise(mean,std)])
+    test_transform = transforms.Compose([transforms.ToTensor(), AddGaussianNoise(mean,std)])
+    root = "./data"
+
+    train_set = torchvision.datasets.FashionMNIST(root=root, download=True, transform=train_transform)
+    test_set = torchvision.datasets.FashionMNIST(root=root, download=True, train=False, transform=test_transform)
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size)
+
+    return train_loader, test_loader
