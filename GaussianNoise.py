a31d458887fc00f2cd373068157ecb24f19ca454
vitalex93 <vitalex93@gmail.com>
2022-06-27 15:07:21 +0300

    gaussian noise visualization

--- GaussianNoise.py
+++ GaussianNoise.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision
 from torchvision import transforms
+import matplotlib.pyplot as plt
+from FashionDataloader import *
 
 class AddGaussianNoise(object):
     def __init__(self, mean=0., std=1.):
@@ -26,3 +28,19 @@ def get_noisy_FashionMNIST_dataloaders(batch_size, mean, std):
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size)
 
     return train_loader, test_loader
+
+corrupted_images_dloader_train = get_noisy_FashionMNIST_dataloaders(18, 0, 0.2)[0]
+
+def plot_dataloader(dataloader, index=0):
+
+    train_features, train_labels = next(iter(dataloader))
+    print(f"Feature batch shape: {train_features.size()}")
+    print(f"Labels batch shape: {train_labels.size()}")
+    img = train_features[index].squeeze()
+    label = train_labels[index]
+    plt.imshow(img, cmap="gray")
+    plt.show()
+    print(f"Label: {label}")
+
+plot_dataloader(get_FashionMNIST_dataloaders(18)[0], 1)
+plot_dataloader(corrupted_images_dloader_train, 1)
