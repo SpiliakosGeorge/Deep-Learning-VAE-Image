5b2f172d2c65813a6d10d0ac5fba3b8990e1b0d8
SpiliakosGeorge <spiliakosgeorge@gmail.com>
2022-07-06 21:07:43 +0300

    removed Sampling.py, deleted plot_dataloader function

--- FashionDataloader.py
+++ FashionDataloader.py
@@ -21,16 +21,3 @@ def get_FashionMNIST_dataloaders(batch_size):
 
     return train_loader, test_loader
 
-# TODO remove plot_dataloader?
-
-def plot_dataloader(dataloader, index=0):
-
-    train_features, train_labels = next(iter(dataloader))
-    print(f"Feature batch shape: {train_features.size()}")
-    print(f"Labels batch shape: {train_labels.size()}")
-    img = train_features[index].squeeze()
-    label = train_labels[index]
-    plt.imshow(img, cmap="gray")
-    plt.show()
-    print(f"Label: {label}")
-
